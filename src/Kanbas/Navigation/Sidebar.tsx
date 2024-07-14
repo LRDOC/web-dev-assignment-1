3c376fc94e0367a9693f3a0bf09dffa501af2d45
Liam O'Connor <107894732+LRDOC@users.noreply.github.com>
2024-07-14 18:31:04 -0400

    reinstalled  them

--- src/Kanbas/Navigation/Sidebar.tsx
+++ src/Kanbas/Navigation/Sidebar.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { AiOutlineDashboard } from 'react-icons/ai';
+import { IoCalendarOutline } from 'react-icons/io5';
+import { LiaBookSolid, LiaCogSolid } from 'react-icons/lia';
+import { FaInbox, FaRegCircleUser } from 'react-icons/fa6';
 import './Navigation.css';
 
 export default function Sidebar() {
     return (
         <div className="sidebar">
-            <Link to="/Kanbas/Dashboard"> Dashboard</Link>
-            <Link to="/Kanbas/Courses"> Courses</Link>
-            <Link to="/Kanbas/Account"> Account</Link>
-            <Link to="/Kanbas/Calendar"> Calendar</Link>
-            <Link to="/Kanbas/Inbox"> Inbox</Link>
-            <Link to="/Kanbas/Settings"> Settings</Link>
+            <Link to="/Kanbas/Dashboard">
+                <span className="icon"><AiOutlineDashboard /></span> Dashboard
+            </Link>
+            <Link to="/Kanbas/Courses">
+                <span className="icon"><LiaBookSolid /></span> Courses
+            </Link>
+            <Link to="/Kanbas/Account">
+                <span className="icon"><FaRegCircleUser /></span> Account
+            </Link>
+            <Link to="/Kanbas/Calendar">
+                <span className="icon"><IoCalendarOutline /></span> Calendar
+            </Link>
+            <Link to="/Kanbas/Inbox">
+                <span className="icon"><FaInbox /></span> Inbox
+            </Link>
+            <Link to="/Kanbas/Settings">
+                <span className="icon"><LiaCogSolid /></span> Settings
+            </Link>
         </div>
     );
 }
