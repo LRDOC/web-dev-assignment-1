9c5c3d6499fd5b0819f52e934f89a68b0246b1bf
Liam O'Connor <107894732+LRDOC@users.noreply.github.com>
2024-07-29 22:13:49 -0400

    fix

--- src/Kanbas/Dashboard/Dashboard.tsx
+++ src/Kanbas/Dashboard/Dashboard.tsx
@@ -1,6 +1,5 @@
-import { Link, useSearchParams } from "react-router-dom";
-import db from "../Database";
-import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import React from "react";
 
 export default function Dashboard({
                                       courses,
